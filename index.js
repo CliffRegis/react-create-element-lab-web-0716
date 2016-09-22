31f86d66e8455cd4af3318a6441dee1fd7d26d05
Clifton Regis <github email address>
2016-09-22 17:03:04 -0400

    completed first React lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Code React element here
+
+const Interests =  
+   	React.createElement('ul', {className: "me_interests"},
+   		[ 
+   			React.createElement("li", {}, "JavaScript"),
+   			React.createElement("li", {}, "React"),
+   			React.createElement("li", {}, "Movies"),
+   			React.createElement("li", {}, "Ice cream"),
+   		]
+   	);
+
+const Tagline = React.createElement('p', {}, "Who is learning React");
+
+const Title = React.createElement('h1', {}, "An Awesome Person");
+
+const meInReact =	React.createElement('div', {className: "me"}, [Title, Tagline, Interests]);
+
+		ReactDOM.render(meInReact, document.getElementById("main"))
+   
